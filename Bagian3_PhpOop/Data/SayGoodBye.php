196f003b70bd708969680c840319a2dfa688ffa9
renfi_26 <108910685+RenaNurhafilah@users.noreply.github.com>
2022-07-27 12:58:09 +0700

    update 27 07 22

--- Bagian3_PhpOop/Data/SayGoodBye.php
+++ Bagian3_PhpOop/Data/SayGoodBye.php
@@ -32,8 +32,27 @@ trait CanRun {
     public abstract function run(): void;
 }
 
-class Person {
-    use sayGoodBye, sayHello, HasName, canRun;
+class ParentPerson {
+    public function goodBye(?string $name): void {
+        echo "Good by in Person" .PHP_EOL;
+    }
+
+    public function hello(?string $name): void {
+        echo "Hello in Person" .PHP_EOL;
+    }
+}
+
+trait All {
+    use sayGoodBye, sayHello, HasName, canRun {
+        // bisa di override
+        // helli is private
+        // goodBye as private
+    }
+}
+
+class Person extends ParentPerson {
+
+    use All;
 
     public function run(): void {
         echo "Person $this->name is running" .PHP_EOL;
